ba918bb4d9c8de32ca7170d5066b1548aea2dc00
dustinapodaca <dustinapodaca@gmail.com>
2022-10-04 14:05:43 -0600

    componetized and made modal form for POST

---
+++ src/components/BookCarousel.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Carousel, Container, Button } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+class BookCarousel extends React.Component {
+  render() {
+    return (
+      <Container>
+        <Carousel className='h-50 rounded-bottom mb-5 bg-dark rounded'>
+          {this.props.books.map((book) => {
+            return (
+              <Carousel.Item>
+                <>
+                  <div className="pb-5 mb-5">
+                    <img className="d-block w-50 mx-auto" src='https://www.nirah.com/images/item-placeholder.svg?id=498f4e96baf0bbbc9351' alt={book.title} />
+                  </div>
+                </>
+                <Carousel.Caption id="carouselText" className='bg-dark mx-auto bg-opacity-75 rounded-bottom'>
+                  <>
+                    <div>
+                      <h3>{book.title}</h3>
+                      <p>{book.description}</p>
+                      <p>{book.status}</p>
+                    </div>
+                    <Button 
+                      variant="danger" 
+                      onClick={() => this.props.deleteBook(book._id)}
+                      >Delete Book
+                    </Button>
+                  </>
+                </Carousel.Caption>
+              </Carousel.Item>
+            )
+          })}
+        </Carousel>
+      </Container>
+    )
+  }
+}
+
+export default BookCarousel;
---
+++ src/components/BookFormModal.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import { Container, Modal, Form, Button } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+class BookFormModal extends React.Component {
+  render() {
+    return (
+      <>
+        <Container>
+          <Modal show={this.props.show} onHide={this.props.handleClose}>
+            <Modal.Header closeButton>
+              <Modal.Title>Add a Book</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              <Form onSubmit={this.props.handleSubmit}>
+                <Form.Group className="mb-3">
+                  <Form.Label>Title</Form.Label>
+                  <Form.Control 
+                    type="text" 
+                    placeholder="Enter Book Title" 
+                    name="title" 
+                  />
+                </Form.Group>
+
+                <Form.Group className="mb-3">
+                  <Form.Label>Description</Form.Label>
+                  <Form.Control 
+                    type="text" 
+                    placeholder="Enter Book Description" 
+                    name="description" 
+                  />
+                </Form.Group>
+
+                <Form.Group className="mb-3">
+                  <Form.Label>Book Status</Form.Label>
+                  <Form.Select id="status" name="status">
+                    <option value="Available">Available</option>
+                    <option value="Low Stock">Low Stock</option>
+                    <option value="Unavailable">Unavailable</option>
+                  </Form.Select>
+                </Form.Group>
+
+                <Button variant="primary" type="submit">
+                  Add Book
+                </Button>
+              </Form>
+            </Modal.Body>
+            <Modal.Footer>
+              <Button variant="secondary" onClick={this.props.handleClose}>
+                Close
+              </Button>
+            </Modal.Footer>
+          </Modal>
+        </Container>
+      </>
+    )
+  }
+}
+
+export default BookFormModal;
